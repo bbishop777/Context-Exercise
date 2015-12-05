2fedbb0d7d89dd61f336f1e1c53f591baf1b594d
BBishop <brad.bishop68@gmail.com>
2015-12-04 19:10:59 -1000

    Completes Context Exercise

--- context.js
+++ context.js
@@ -1,7 +1,7 @@
 var Robot = function (name) {
   this.name = name;
 
-}
+};
 
 function add ( op1, op2 ) {
   this.name = this.name || "Humans";
@@ -9,22 +9,25 @@ function add ( op1, op2 ) {
 }
 
 var voltron = new Robot("Voltron");
-var bender = new Robot("Bender Rodriguez");
+var bender =  new Robot("Bender Rodriguez");
 var optimus = new Robot("Optimus Prime");
 var megaman = new Robot("Mega Man");
 var bmo = new Robot("B-Mo");
 var wall_e = new Robot("Wall-E");
 
-
 // #1
-console.log(  );
+console.log(add(0,1) );
 
-// #2  
-console.log(  );
+// #2
+console.log(add.call(voltron, [2, 3]));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus, [20, 30]));
 
 // #4
 
-console.log(  );
+
+var calculate = add.bind(bender);
+console.log(calculate("drinking", "beer"));
+
+
